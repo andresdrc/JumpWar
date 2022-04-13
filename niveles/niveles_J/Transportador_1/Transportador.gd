40d2258981bdc456d144378d8692bddada9b3209
Efrain Viscarra <ej56aoe@gmail.com>
2022-04-13 13:25:39 -0400

    se modifico en transportador_1 de derecha a izquierda

--- niveles/niveles_J/Transportador_1/Transportador.gd
+++ niveles/niveles_J/Transportador_1/Transportador.gd
@@ -18,10 +18,23 @@ func _ready() -> void:
  
 func _init_twen() -> void: 
 	#mover_a.x = GLOBAL.random(2,6)
+	var ran = RandomNumberGenerator.new()
+	ran.randomize()
+	var num= int (ran.randf_range(1.0,3.0))
+	print(num)
 	mover_a = mover_a * tama_cell 
 	var durac = mover_a.length() / velocidadT 
 	tween.interpolate_property(self,"seguir", Vector2.ZERO, mover_a,durac,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT, dur_inact)
 	tween.interpolate_property(self,"seguir", mover_a, Vector2.ZERO,durac,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT, durac + dur_inact * 2)
+	if num == 1.0:
+		tween.interpolate_property(self,"seguir", Vector2.ZERO, mover_a,durac,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT, dur_inact)
+		tween.interpolate_property(self,"seguir", mover_a, Vector2.ZERO,durac,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT, durac + dur_inact * 2)
+		print("izquierda")
+	else:
+		tween.interpolate_property(self,"seguir", Vector2.ZERO, -mover_a,durac,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT, dur_inact)
+		tween.interpolate_property(self,"seguir", -mover_a, Vector2.ZERO,durac,Tween.TRANS_LINEAR,Tween.EASE_IN_OUT, durac + dur_inact * 2)
+		print('derecha')
+	
 	tween.start()
 
 func _physics_process(delta):
